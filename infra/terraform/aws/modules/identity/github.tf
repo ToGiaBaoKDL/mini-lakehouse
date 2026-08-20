19d1550ec431ed8c8235637fd8f58c46dd5ea28b
To Gia Bao <bao.to@timo.vn>
2026-08-20 13:08:20 +0700

    fix(observability): fix state bucket name to tgbao-terraform-state in CI and IAM policy
    - Fix state bucket name from tgbao-dev-terraform-state to tgbao-terraform-state in deploy-signoz.yml
    - Allow *-terraform-state-${var.account_id} in github-signoz-deployer IAM policy

--- infra/terraform/aws/modules/identity/github.tf
+++ infra/terraform/aws/modules/identity/github.tf
@@ -121,8 +121,8 @@ data "aws_iam_policy_document" "github_signoz_deployer" {
       "s3:DeleteObject",
     ]
     resources = [
-      "arn:${data.aws_partition.current.partition}:s3:::${var.name_prefix}-terraform-state-${var.account_id}",
-      "arn:${data.aws_partition.current.partition}:s3:::${var.name_prefix}-terraform-state-${var.account_id}/lakehouse/signoz/*",
+      "arn:${data.aws_partition.current.partition}:s3:::*-terraform-state-${var.account_id}",
+      "arn:${data.aws_partition.current.partition}:s3:::*-terraform-state-${var.account_id}/lakehouse/signoz/*",
     ]
   }
 }
